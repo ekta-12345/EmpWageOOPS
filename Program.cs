dc3477a21360bdb2e0026c5051beaa137bd37abd
ekta12345 <ektakumbhare3@gmail.com>
2021-04-16 17:49:19 +0530

    Added code for checking Emplloyee is PartTime or Full Time

--- Program.cs
+++ Program.cs
@@ -2,13 +2,15 @@
 
 namespace EmpWageOOPS
 {
-    class UC2DailyWage
+    class UC3EmpPartTime
     {
-        public void EmployeeWage()
+        public void ParttimeEmployeeWage()
 
         {
 
-            int PRESENT = 1;
+            int FULL_TIME = 1;
+
+            int PART_TIME = 2;
 
             int EMP_RATE_PER_HOUR = 20;
 
@@ -18,15 +20,23 @@ namespace EmpWageOOPS
 
             Random random = new Random();
 
-            int EmpCheck = random.Next(0, 2);
+            int EmpCheack = random.Next(0, 3);
 
-            if (EmpCheck == PRESENT)
+            if (EmpCheack == FULL_TIME)
 
             {
 
                 emphrs = 8;
 
 
+
+            }
+
+            else if (EmpCheack == PART_TIME)
+
+            {
+
+                emphrs = 4;
 
             }
 
@@ -43,12 +53,16 @@ namespace EmpWageOOPS
             Console.WriteLine("Employe Wage Per Day:- " + empWage);
 
         }
-        static void Main(string[] args)
+
+    
+    static void Main(string[] args)
         {
             //    UC1EmpCheck uC1 = new UC1EmpCheck();
             //    uC1.PresentorAbsent();
-            UC2DailyWage uC2 = new UC2DailyWage();
-            uC2.EmployeeWage();
+            //UC2DailyWage uC2 = new UC2DailyWage();
+            //uC2.EmployeeWage();
+            UC3EmpPartTime uC3 = new UC3EmpPartTime();
+            uC3.ParttimeEmployeeWage();
 
         }
     }
