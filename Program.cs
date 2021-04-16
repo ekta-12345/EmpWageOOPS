dcb42ce30a476c53c014ac155b8ec8e3d170bcc6
ekta12345 <ektakumbhare3@gmail.com>
2021-04-16 18:30:21 +0530

    Added code for Managing Employee wage of Multiple companies

--- Program.cs
+++ Program.cs
@@ -2,133 +2,93 @@
 
 namespace EmpWageOOPS
 {
-    class UC9StoreWageMultpleCompanies
+    class UC10ManageEmplWageOfMultipleCompanies
     {
-        public const int FULL_TIME = 1; //Constant variable
-
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalempwage;
+        public UC10ManageEmplWageOfMultipleCompanies(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int TotalEmpWage)
+        {
+            this.totalempwage = totalempwage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company :- " + company + " is :- " + totalempwage;
+        }
+    }
+    public class EmpWageBuilderArray
+    {
+        public const int FULL_TIME = 1;     //Constant variable
         public const int PART_TIME = 2;
 
+        public int numofCompany = 0;
+        private UC10ManageEmplWageOfMultipleCompanies[] UC10ManageEmplWageOfMultipleCompaniesArray;
 
-
-        private string company;
-
-        private int empRatePerHour;
-
-        private int numOfWorkingDays;
-
-        private int maxHoursPerMonth;
-
-        private string totalempwage;
-
-
-
-        public UC9StoreWageMultpleCompanies(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-
+        public EmpWageBuilderArray()
         {
-
-            this.company = company;
-
-            this.empRatePerHour = empRatePerHour;
-
-            this.numOfWorkingDays = numOfWorkingDays;
-
-            this.maxHoursPerMonth = maxHoursPerMonth;
-
-
-
+            this.UC10ManageEmplWageOfMultipleCompaniesArray = new UC10ManageEmplWageOfMultipleCompanies[5];
         }
-
-        public void ComputeEmployeeWage()
-
+        public void addComapnyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-
+            UC10ManageEmplWageOfMultipleCompaniesArray[this.numofCompany] = new UC10ManageEmplWageOfMultipleCompanies(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            numofCompany++;
+        }
+        public void ComputeEmployeeWage()
+        {
+            for (int i = 0; i < numofCompany; i++)
+            {
+                UC10ManageEmplWageOfMultipleCompaniesArray[i].setTotalEmpWage(this.ComputeEmployeeWage(this.UC10ManageEmplWageOfMultipleCompaniesArray[i]));
+                Console.WriteLine(this.UC10ManageEmplWageOfMultipleCompaniesArray[i].toString());
+            }
+        }
+        private int ComputeEmployeeWage(UC10ManageEmplWageOfMultipleCompanies UC10ManageEmplWageOfMultipleCompanies)
+        {
             int emphrs = 0;
-
             int empWage = 0;
-
             int totalempwage = 0;
-
             int hrs = 0;
-
             int workingDays = 1;
-
-            Random random = new Random(); //Random Class
-
-            while (hrs < this.empRatePerHour && this.numOfWorkingDays <= this.maxHoursPerMonth)
-
-            // for (int Day = 0; Day < NUM_OF_WORKING_DAYS; Day++)
-
+            while (emphrs <= UC10ManageEmplWageOfMultipleCompanies.maxHoursPerMonth && workingDays < UC10ManageEmplWageOfMultipleCompanies.numOfWorkingDays)
             {
 
-
-
                 workingDays++;
-
-                int EmpCheack = random.Next(0, 3); //Random Generate 0 ,1,2
-
-                switch (EmpCheack) //Switch case Statment
-
+                Random random = new Random();
+                int EmpCheack = random.Next(0, 3);      //Random Generate 0 ,1,2
+                switch (EmpCheack)          //Switch case Statment
                 {
-
-                    case FULL_TIME: //Employee is FullTime=1
-
+                    case FULL_TIME:         //Employee is FullTime=1
                         emphrs = 8;
-
                         break;
-
-                    case PART_TIME: //Employee is FullTime=2
-
+                    case PART_TIME:          //Employee is FullTime=2
                         emphrs = 4;
-
                         break;
-
                     default:
-
                         emphrs = 0;
-
                         break;
-
                 }
+                hrs += emphrs;
 
+                Console.WriteLine("Day " + workingDays + " Emp hrs:- " + emphrs);//Display empwage
 
-
-                // Calculate empWage
-
-                hrs += emphrs; //Display empwage
-
-                totalempwage = totalempwage + empWage; //Calculate total employe month wage
-
-                empWage = empRatePerHour * emphrs;
+                //totalempwage = totalempwage + empWage;      //Calculate total employe month wage
+                //empWage = empRatePerHour * emphrs;
 
             }
+            return emphrs * UC10ManageEmplWageOfMultipleCompanies.empRatePerHour;
 
 
 
-            // Console.WriteLine($"Toatal Emp wage for company:- {company} is {totalempwage}");
-
-            Console.WriteLine("Employe Wage Per Day :- " + empWage);
-
-            //Console.WriteLine("Total Employe Month Wage :- " + totalempwage);
-
-            // Console.WriteLine("Employee wage for " + workingDays + " days " + totalempwage);
-
-            Console.WriteLine("Working hours " + hrs + "\n");
-
-            //return totalempwage;
 
         }
-
-        public string toString()
-
-        {
-
-            return "Total Emp Wage for company :- " + company + "is :- " + totalempwage;
-
-        }
-
-
-
-
 
 
 
@@ -154,12 +114,16 @@ namespace EmpWageOOPS
             //UC8WageMultipleCompanies uC8 = new UC8WageMultipleCompanies();
             //UC8WageMultipleCompanies.ComputeEmpWage("Amazon",20,4,20);
             //UC8WageMultipleCompanies.ComputeEmpWage("Infosys", 30, 6, 20);
-            UC9StoreWageMultpleCompanies Dmart = new UC9StoreWageMultpleCompanies("Dmart", 20, 4, 20);
-            UC9StoreWageMultpleCompanies Reliance = new UC9StoreWageMultpleCompanies("Reliance", 30, 5, 20);
-            Dmart.ComputeEmployeeWage(); //call method
-            Console.WriteLine(Dmart.toString());
-            Reliance.ComputeEmployeeWage();//call method
-            Console.WriteLine(Reliance.toString());
+            //UC9StoreWageMultpleCompanies Dmart = new UC9StoreWageMultpleCompanies("Dmart", 20, 4, 20);
+            //UC9StoreWageMultpleCompanies Reliance = new UC9StoreWageMultpleCompanies("Reliance", 30, 5, 20);
+            //Dmart.ComputeEmployeeWage(); //call method
+            //Console.WriteLine(Dmart.toString());
+            //Reliance.ComputeEmployeeWage();//call method
+            //Console.WriteLine(Reliance.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();   //Create object of class
+            empWageBuilder.addComapnyEmpWage("Dmart", 20, 2, 10);  //call method
+            empWageBuilder.addComapnyEmpWage("Reliance", 10, 4, 20); //call method
+            empWageBuilder.ComputeEmployeeWage(); //call method
         }
     }
 }
