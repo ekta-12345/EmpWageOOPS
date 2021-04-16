b6f91df7201d92e3406f26b293de28de71b5e378
ekta12345 <ektakumbhare3@gmail.com>
2021-04-16 18:07:38 +0530

    Added code to compute Employee wage till max working hours is reached

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 namespace EmpWageOOPS
 {
-    class UC5WagesForMonth
+    class UC6WageMaxHours
     {
         public const int FULL_TIME = 1; //Constant variable
 
@@ -12,7 +12,11 @@ namespace EmpWageOOPS
 
         public const int NUM_OF_WORKING_DAYS = 20;
 
-        public static void WageForMonth()
+        public const int MAX_WORKING_HRS = 100;
+
+        public const int MAX_WORKING_DAYS = 20;
+
+        public static void WagesCondition()
 
         {
 
@@ -22,11 +26,21 @@ namespace EmpWageOOPS
 
             int totalempwage = 0;
 
+            int hrs = 0;
+
+            int workingDays = 1;
+
             Random random = new Random(); //Random Class
 
-            for (int Day = 0; Day < NUM_OF_WORKING_DAYS; Day++)
+            while (hrs < MAX_WORKING_HRS && workingDays <= MAX_WORKING_DAYS)
+
+            // for (int Day = 0; Day < NUM_OF_WORKING_DAYS; Day++)
 
             {
+
+
+
+                workingDays++;
 
                 int EmpCheack = random.Next(0, 3); //Random Generate 0 ,1,2
 
@@ -58,7 +72,7 @@ namespace EmpWageOOPS
 
                 empWage = EMP_RATE_PER_HOUR * emphrs; // Calculate empWage
 
-                //Display empwage
+                hrs += emphrs; //Display empwage
 
                 totalempwage = totalempwage + empWage; //Calculate total employe month wage
 
@@ -68,8 +82,12 @@ namespace EmpWageOOPS
 
             Console.WriteLine("Total Employe Month Wage :- " + totalempwage);
 
+            Console.WriteLine("Employee wage for " + workingDays + " days " + totalempwage);
+
+            Console.WriteLine("Working hours " + hrs);
 
         }
+
 
         static void Main(string[] args)
         {
@@ -81,9 +99,12 @@ namespace EmpWageOOPS
             //uC3.ParttimeEmployeeWage();
             //UC4SwitchCase uC4 = new UC4SwitchCase();
             //UC4SwitchCase.SwitchCaseStatement();
-            UC5WagesForMonth uC5 = new UC5WagesForMonth();
-            UC5WagesForMonth.WageForMonth();
+            //UC5WagesForMonth uC5 = new UC5WagesForMonth();
+            //UC5WagesForMonth.WageForMonth();
+            UC6WageMaxHours uC6 = new UC6WageMaxHours();
+            UC6WageMaxHours.WagesCondition();
         }
     }
 }
+
 
