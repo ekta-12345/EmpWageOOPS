2d9dded7dc7652d3a464e1fa0a205649bfafd171
ekta12345 <ektakumbhare3@gmail.com>
2021-04-16 17:54:22 +0530

    Employee wage using Switch case

--- Program.cs
+++ Program.cs
@@ -2,67 +2,69 @@
 
 namespace EmpWageOOPS
 {
-    class UC3EmpPartTime
+    class UC4SwitchCase
     {
-        public void ParttimeEmployeeWage()
+        public const int FULL_TIME = 1; //Constant variable
+
+        public const int PART_TIME = 2;
+
+        public const int EMP_RATE_PER_HOUR = 20;
+
+        public static void SwitchCaseStatement()
 
         {
-
-            int FULL_TIME = 1;
-
-            int PART_TIME = 2;
-
-            int EMP_RATE_PER_HOUR = 20;
 
             int emphrs = 0;
 
             int empWage = 0;
 
-            Random random = new Random();
+            Random random = new Random(); //Random Class
 
-            int EmpCheack = random.Next(0, 3);
+            int EmpCheack = random.Next(0, 3); //Random Generate 0 ,1,2
 
-            if (EmpCheack == FULL_TIME)
-
-            {
-
-                emphrs = 8;
-
-
-
-            }
-
-            else if (EmpCheack == PART_TIME)
+            switch (EmpCheack) //Switch case Statment
 
             {
 
-                emphrs = 4;
+                case FULL_TIME: //Employee is FullTime=1
+
+                    emphrs = 8;
+
+                    break;
+
+                case PART_TIME: //Employee is FullTime=2
+
+                    emphrs = 4;
+
+                    break;
+
+                default:
+
+                    emphrs = 0;
+
+                    break;
 
             }
 
-            else
 
-            {
 
-                emphrs = 0;
+            empWage = EMP_RATE_PER_HOUR * emphrs; // Calculate empWage
 
-            }
-
-            empWage = EMP_RATE_PER_HOUR * emphrs;
-
-            Console.WriteLine("Employe Wage Per Day:- " + empWage);
+            Console.WriteLine("Employe Wage Per Day:- " + empWage); //Display empwage
 
         }
 
-    
-    static void Main(string[] args)
+
+        static void Main(string[] args)
         {
             //    UC1EmpCheck uC1 = new UC1EmpCheck();
             //    uC1.PresentorAbsent();
             //UC2DailyWage uC2 = new UC2DailyWage();
             //uC2.EmployeeWage();
-            UC3EmpPartTime uC3 = new UC3EmpPartTime();
-            uC3.ParttimeEmployeeWage();
+            //UC3EmpPartTime uC3 = new UC3EmpPartTime();
+            //uC3.ParttimeEmployeeWage();
+            UC4SwitchCase uC4 = new UC4SwitchCase();
+            UC4SwitchCase.SwitchCaseStatement();
 
         }
     }
