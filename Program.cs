6b4600f670f03ae64fd9a696e61ce3affca9450c
ekta12345 <ektakumbhare3@gmail.com>
2021-04-16 17:38:24 +0530

    Added code for checking Employee present or absent

--- Program.cs
+++ Program.cs
@@ -4,13 +4,39 @@ namespace EmpWageOOPS
 {
     class UC1EmpCheck
     {
-       
+        public void PresentorAbsent()
+
+        {
+            Random random = new Random();
+
+            int EmpCheck = random.Next(0, 2);
+
+            if (EmpCheck == 1)
+
+            {
+
+                Console.WriteLine("Employee Is Present");
+
+
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Employe Is Absent");
+
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            
+            UC1EmpCheck uC1 = new UC1EmpCheck();
+            uC1.PresentorAbsent();
         }
 
     }
-   
+
 }
+
